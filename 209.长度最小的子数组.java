3c4c1deb60a0870e5f287dd95fc0ecde3c1376f9
Switcherman <418211500@163.com>
2020-04-26 15:21:23 +0800

    每日一题

---
+++ 209.长度最小的子数组.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=209 lang=java
+ *
+ * [209] 长度最小的子数组
+ */
+
+// @lc code=start
+class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        int result = Integer.MAX_VALUE;
+        int left = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            while (sum >= s) {
+                result = Math.min(result, i - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return result == Integer.MAX_VALUE ? 0 : result;
+    }
+}
+// @lc code=end
+
